8e6e16ed4effc3bb3a79ce48525106794f5554d2
Abdur Rahman Mansoor <armansoor2016@gmail.com>
2021-04-07 16:33:35 +0530

    fourth commit

--- mail.php
+++ mail.php
@@ -4,10 +4,13 @@ $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "armansoorofficial@gmail.com";
-$subject = "Mail From armansoor's blog";
+$subject = "Mail From armansoor";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From:armansoor156@gmail.com" . "\r\n" .
+$headers = "From: contact@armansoor.ezyro.com" . "\r\n" .
+"CC: armansoor2016@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
+//redirect
+header("Location:thankyou.html");
 ?>
