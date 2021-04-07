bf05d92cbd0b4067cb6e0fcaca250b93883d7017
Abdur Rahman Mansoor <armansoor2016@gmail.com>
2021-04-07 10:29:03 +0530

    Initial commit

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "armansoorofficial@gmail.com";
+$subject = "Mail From armansoor's blog";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From:armansoor156@gmail.com" . "\r\n" .
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+?>
