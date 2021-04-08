0714c984d94cdd80815135008cb205a113f2774b
Abdur Rahman Mansoor <armansoor2016@gmail.com>
2021-04-08 12:34:14 +0530

    12:34

--- mail.php
+++ mail.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $name = $_POST['name'];
 $email= $_POST['email'];
@@ -13,20 +12,4 @@ if($email!=NULL){
 }
 //redirect
 header("Location:thankyou.html");
-=======
-<?php
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "armansoorofficial@gmail.com";
-$subject = "Mail From armansoor";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: contact@armansoor.ezyro.com" . "\r\n" .
-"CC: armansoor2016@gmail.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:thankyou.html");
->>>>>>> 96a379d98e278c596d94b548a8aad77c7593eb51
 ?>
